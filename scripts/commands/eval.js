e7362111b80f0bb972725d0f57f5adb232b29ba8
Priyanshi Kaur <sasmitapattnayak104@gmail.com>
2025-05-09 17:41:43 +0530

    Update eval.js

--- scripts/commands/eval.js
+++ scripts/commands/eval.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     
     onStart: async function({ msg, bot, args, chatId, userId, config, botName, senderName, username, copyrightMark, threadModel, userModel, user, thread, api }) {
-        const owner = '5947023314'; // Replace with your Telegram user ID
+        const owner = '6388959089'; // Replace with your Telegram user ID
         if (userId.toString() !== owner) {
             return bot.sendMessage(chatId, 'You do not have permission to use this command.', { replyToMessage: msg.message_id });
         }
